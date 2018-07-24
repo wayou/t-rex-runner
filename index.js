a9bce201828e16c9a319c2968762f437d6435cbb
Alexey Bityukov <alex.bityuckov@gmail.com>
2018-07-24 22:04:03 +0300

    fix bug on rule for insert style

--- index.js
+++ index.js
@@ -467,7 +467,13 @@
                     'from { width:' + Trex.config.WIDTH + 'px }' +
                     'to { width: ' + this.dimensions.WIDTH + 'px }' +
                     '}';
-                document.styleSheets[0].insertRule(keyframes, 0);
+
+                var animationDefinition = document.createElement("style");
+                document.head.appendChild(animationDefinition);
+
+                animationDefinition.sheet.insertRule(keyframes, 0);
+
+
 
                 this.containerEl.addEventListener(Runner.events.ANIM_END,
                     this.startGame.bind(this));
